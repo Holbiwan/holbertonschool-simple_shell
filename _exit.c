ed0f57c3e1d3b7b65439cb8dc2f900594a53d0bd
Holbiwan <6188@holbertonstudents.com>
2023-08-22 22:47:53 -0700

    fix betty

--- _exit.c
+++ _exit.c
@@ -1,14 +1,15 @@
 #include "shell.h"
-/*
- * _exit:immediately terminates the process without performing normal cleanup
- *
+
+/**
+ * _exit2:Terminates the process after printing a message
  * Return: 0
-*/
+**/
 
 int _exit2(void)
 {
 	printf("Before _exit\n");
 	_exit(0);
+
 	printf("After _exit\n");
 
 	return (0);
